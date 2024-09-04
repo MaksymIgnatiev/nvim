6ec6de29e7c47b537d707fbadb28339f3763bd0e
MaksymIgnatievOfficial <maksym.ignatiev2006@gmail.com>
2024-09-05 00:10:49 +0200

    Oh, forgot one file!

--- lua/plugins/config/cmp.lua
+++ lua/plugins/config/cmp.lua
@@ -1,24 +1,24 @@
 -- ~/.config/nvim/lua/plugins/config/cmp.lua
 
--- local cmp = require('cmp')
--- 
--- cmp.setup({
-  -- snippet = {
-    -- expand = function(args)
-      -- require('luasnip').lsp_expand(args.body)
-    -- end,
-  -- },
-  -- mapping = {
-    -- ['<C-Space>'] = cmp.mapping.complete(),
-    -- ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    -- ['<Tab>'] = cmp.mapping.select_next_item(),
-    -- ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-  -- },
-  -- sources = cmp.config.sources({
-    -- { name = 'nvim_lsp' },
-    -- { name = 'luasnip' },
-  -- }, {
-    -- { name = 'buffer' },
-    -- { name = 'path' },
-  -- })
--- })
+local cmp = require('cmp')
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  }, {
+    { name = 'buffer' },
+    { name = 'path' },
+  })
+})
