34417e20a2cc0584407d42a950b3a2b5fdf2b0d7
MaksymIgnatievOfficial <maksym.ignatiev2006@gmail.com>
2024-09-05 10:58:42 +0200

    Added dynamicaly detection of ~ directory (for compatitability)

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 -- ~/.config/nvim/init.lua
 
-package.path = package.path ..";/home/arrow_function/.config/nvim/lua/?/init.lua;/home/arrow_function/.config/nvim/lua/plugins/?.lua;/home/arrow_function/.config/nvim/lua/plugins/config/?.lua;/home/arrow_function/.config/nvim/lua/global/?.lua"
+local home = os.getenv("HOME")
+
+package.path = package.path .. ";" .. home .. "/.config/nvim/lua/?/init.lua;" .. home .. "/.config/nvim/lua/plugins/?.lua;" .. home .. "/.config/nvim/lua/plugins/config/?.lua;" .. home .. "/.config/nvim/lua/global/?.lua"
 
 -- Load all the configuration
 
