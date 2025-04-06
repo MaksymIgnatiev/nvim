1140db776d0934feba32f6ec52a888330a94e873
MaksymIgnatiev <maksym.ignatiev2006@gmail.com>
2025-04-06 23:55:03 +0200

    [plugins/config/nvim-colorizer] Removed redundant autocmd that overloaded nvim when opening files more than 100kb. Code was generated by LLM when i tried to figure out why plugin doesn't work properly (solved somehow)

--- lua/plugins/config/nvim-colorizer.lua
+++ lua/plugins/config/nvim-colorizer.lua
@@ -1,36 +1,30 @@
--- #f00
-require('colorizer').setup(
-{
-	-- Specific files
-	-- use "!name" to exclude a file
-	'*', -- Highlight all files, but you can specify specific file types like 'css', 'javascript', etc.
-},
-{
-	-- Global
-	names = false, -- "Names" such as `Green`, `Blue`, `Gray`, `Magenta`, etc
-	rgb_fn = true,
-	RRGGBBAA = true,        -- #RRGGBBAA hex codes
-	hsl_fn   = true,        -- CSS hsl() and hsla() functions
-}
+local colorizer = require('colorizer')
+
+colorizer.setup(
+	{
+		-- Specific files
+		-- use "!name" to exclude a file
+		-- You can specify specific file types like 'css', 'javascript', etc.
+		"*"
+	},
+	{
+		-- Global
+		names    = false, -- "Names" such as `Green`, `Blue`, `Gray`, `Magenta`, etc
+		rgb_fn   = true,
+		RRGGBBAA = true, -- #RRGGBBAA hex codes
+		hsl_fn   = true, -- CSS hsl() and hsla() functions
+	}
 )
 
 -- DEFAULT_OPTIONS = {
-	-- RGB      = true;         -- #RGB hex codes
-	-- RRGGBB   = true;         -- #RRGGBB hex codes
-	-- names    = true;         -- "Name" codes like Blue
-	-- RRGGBBAA = false;        -- #RRGGBBAA hex codes
-	-- rgb_fn   = false;        -- CSS rgb() and rgba() functions
-	-- hsl_fn   = false;        -- CSS hsl() and hsla() functions
-	-- css      = false;        -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-	-- css_fn   = false;        -- Enable all CSS *functions*: rgb_fn, hsl_fn
-	-- -- Available modes: foreground, background
-	-- mode     = 'background'; -- Set the display mode.
+-- RGB      = true;         -- #RGB hex codes
+-- RRGGBB   = true;         -- #RRGGBB hex codes
+-- names    = true;         -- "Name" codes like Blue
+-- RRGGBBAA = false;        -- #RRGGBBAA hex codes
+-- rgb_fn   = false;        -- CSS rgb() and rgba() functions
+-- hsl_fn   = false;        -- CSS hsl() and hsla() functions
+-- css      = false;        -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+-- css_fn   = false;        -- Enable all CSS *functions*: rgb_fn, hsl_fn
+-- -- Available modes: foreground, background
+-- mode     = 'background'; -- Set the display mode.
 -- }
-
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*",
-  callback = function()
-    require('colorizer').attach_to_buffer()
-  end,
-})
-
