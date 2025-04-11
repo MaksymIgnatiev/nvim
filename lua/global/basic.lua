edfcbb225f762a55055b86d43eff664b2983496b
MaksymIgnatiev <maksym.ignatiev2006@gmail.com>
2025-04-11 22:22:39 +0200

    [global] Changed: basic.lua, theme/global.lua files. Added configuraton for the airline statusbar in the basic.lua file. Moved highlight groups for git into theme/global.lua file

--- lua/global/basic.lua
+++ lua/global/basic.lua
@@ -1,32 +1,30 @@
+vim.g.airline_extensions_whitespace_enabled = 0
+vim.g.airline_extensions_whitespace_checks = {}
+vim.g.airline_extensions_whitespace_mixed_indent_algo = 2
+
+vim.g.airline_powerline_fonts = 1
+vim.g.mapleader = ' '
+vim.g.tmux_navigator_verbose = 1
+vim.o.autoindent = true
+vim.o.clipboard = "unnamed"
+vim.o.ignorecase = true
+vim.o.mouse = 'a'
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.autoindent = true
-vim.o.tabstop = 4
+vim.o.scrolloff = 8
+vim.o.shell = '/bin/zsh'
 vim.o.shiftwidth = 4
+vim.o.signcolumn = "yes"
+vim.o.smartcase = true
 vim.o.smarttab = true
 vim.o.softtabstop = 4
-vim.o.mouse = 'a'
-vim.o.shell = '/bin/zsh'
 vim.o.syntax = 'on'
-vim.g.mapleader = ' '
-vim.g.airline_powerline_fonts = 1
+vim.o.tabstop = 4
 vim.o.termguicolors = true
-vim.opt.signcolumn = "yes"
-vim.g.tmux_navigator_verbose = 1
-vim.opt.scrolloff = 8
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
 vim.o.wrap = true
-vim.opt.clipboard = "unnamedplus"
-
-vim.cmd [[
-  highlight GitGutterAdd guifg=#00FF00 guibg=NONE
-  highlight GitGutterChange guifg=#FFFF00 guibg=NONE
-  highlight GitGutterDelete guifg=#FF0000 guibg=NONE
-]]
 
 vim.diagnostic.config({
-    update_in_insert = true,
-    signs = true,
-    underline = true,
+	update_in_insert = true,
+	signs = true,
+	underline = true,
 })
--- lua/global/theme/global.lua
+++ lua/global/theme/global.lua
@@ -1,4 +1,7 @@
 local theme = require("global.theme.config")
-local link = theme.link
+local highlight, link = theme.highlight, theme.link
 
 link("DiagnosticUnnecessary", "LspInlayHint")
+highlight("GitGutterAdd", "#00FF00")
+highlight("GitGutterChange", "#FFFF00")
+highlight("GitGutterDelete", "#FF0000")
