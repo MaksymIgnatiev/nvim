9eee89c79175cf3fc5703a0ad6e504f60124a2ba
MaksymIgnatiev <maksym.ignatiev2006@gmail.com>
2025-05-08 23:42:00 +0200

    [plugins/plugin_list.lua] Added 'kndndrj/nvim-dbee' plugin

--- lua/plugins/plugins_list.lua
+++ lua/plugins/plugins_list.lua
@@ -47,7 +47,8 @@ local plugins = {
 	'williamboman/mason-lspconfig.nvim',
 	'williamboman/mason.nvim',
 	'folke/noice.nvim',
-	'MunifTanjim/nui.nvim'
+	'MunifTanjim/nui.nvim',
+	{ 'kndndrj/nvim-dbee', run = function () require("dbee").install() end }
 }
 
 return plugins
